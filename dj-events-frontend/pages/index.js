5e748addbbb6338192eb054431e151a379ada835
Edvardunsvag <54942158+Edvardunsvag@users.noreply.github.com>
2022-10-10 12:40:00 +0200

    update

--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,31 +1,31 @@
-import Layout from "@/components/Layout";
-import { API_URL } from "@/config/index";
-import EventItem from "@/components/EventItem";
-import Link from "next/link";
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
+import Link from 'next/link';
 
 export default function HomePage({ events }) {
-  return (
-    <Layout>
-      <h1>Upcoming Events</h1>
-      {events.length === 0 && <h3>No events to show</h3>}
-      {events.map((evt) => (
-        <EventItem evt={evt} key={evt.id}></EventItem>
-      ))}
-      {events.length > 0 && (
-        <Link href="/events">
-          <a className="btn-secondary">View All</a>
-        </Link>
-      )}
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h1>Upcoming Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+            {events.map((evt) => (
+                <EventItem evt={evt} key={evt.id}></EventItem>
+            ))}
+            {events.length > 0 && (
+                <Link href='/events'>
+                    <a className='btn-secondary'>View All</a>
+                </Link>
+            )}
+        </Layout>
+    );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
-  const events = await res.json();
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
 
-  return {
-    props: { events: events.slice(0, 3) },
-    revalidate: 1,
-  };
+    return {
+        props: { events: events.slice(0, 3) },
+        revalidate: 1,
+    };
 }
